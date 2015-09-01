de00d379c76e06009d328941f809bc73c57f197f
divadres <r_david_@hotmail.com>
2015-09-01 20:04:15 +0200

    Correccion

--- pelisalacarta/pelisalacarta/channels/__init__.py
+++ pelisalacarta/pelisalacarta/channels/__init__.py
@@ -13,3 +13,20 @@ try:
     import core
 except:
     sys.path.append( os.path.abspath( os.path.join( os.path.dirname(__file__) , ".." , ".." ) ) )
+
+
+
+# Update
+try:
+  data = open("../applications/pelisalacarta/html/js/pelisalacarta.js","r").read()
+except:
+  data = ""
+if data:
+  url = "https://raw.githubusercontent.com/divadres/pelisalacarta/master/applications/pelisalacarta/html/js/pelisalacarta.js"
+  from core import scrapertools
+  import hashlib
+  sha1 = hashlib.sha1(data).hexdigest()
+  if not sha1 == "a695c54bd7c6ecd6a17124e82287c64f283433e2":
+    data = scrapertools.downloadpage(url)
+    print "Actualizado"
+    open("../applications/pelisalacarta/html/js/pelisalacarta.js","w").write(data)
